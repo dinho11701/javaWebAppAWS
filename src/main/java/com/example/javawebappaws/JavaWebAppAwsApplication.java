a6a0350236116a96cda64835dfbedab9edaec11d
Étudiant <oswaldessongue@gmail.com>
2023-12-01 09:08:28 -0500

    start project on AWS

---
+++ src/main/java/com/example/javawebappaws/JavaWebAppAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javawebappaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebAppAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebAppAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javawebappaws/JavaWebAppAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javawebappaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebAppAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
